f9a47eea8b6990e659e2f306f3be1a796ea647d5
Christopher Saunders <topher6345@gmail.com>
2022-12-04 23:14:28 -0800

    1670224468

--- day-5-part-2.rb
+++ day-5-part-2.rb
@@ -18,15 +18,8 @@ move 2 from 2 to 1
 move 1 from 1 to 2
 INPUT
 
-
-
-
-/(?<foo>\s{3})|(?<bar>\S{3})/ =~ "    [D]   "
-bar
-
 def main(input)
   state, instructions = input.split("\n\n")
-
 
   stacks = []
 
@@ -38,7 +31,6 @@ def main(input)
   end
   stacks.unshift nil
   
-
   instructions.each_line(chomp: true) do |line|
     /move (?<count>\d+) from (?<from>\d+) to (?<to>\d+)/ =~ line
 
